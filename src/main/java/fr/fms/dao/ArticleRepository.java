98cf4d0980bd6899a95527a7bf90e8d9e0c84dd4
DCluseau <daphne.cluseau@fms-ea.com>
2026-03-25 09:23:46 +0100

    Program from masterclass

---
+++ src/main/java/fr/fms/dao/ArticleRepository.java
@@ -0,0 +1,15 @@
+package fr.fms.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.fms.entities.Article;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+	public List<Article> findByBrand(String brand);
+	public List<Article> findByBrandContains(String brand);
+	public List<Article> findByBrandAndPrice(String brand, double price);
+	public List<Article> findByCategoryId(Long categoryId);
+//	public List<Article> searchArticles(@Param("x") String kw, @Param("y")double price);
+}
